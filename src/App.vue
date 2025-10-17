7e52eeaef5bf33ad8769579076c33b8eb78e9e5f
MateoGs013 <mateogabus@gmail.com>
2025-10-17 10:09:34 -0300

    chupalaaaaaa

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,39 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+const personajes = [
+  {
+    id: 1,
+    nombre: 'Personaje 1',
+    descripcion: 'Descripción del personaje 1',
+  },
+  {
+    id: 2,
+    nombre: 'Personaje 2',
+    descripcion: 'Descripción del personaje 2',
+  },
+  {
+    id: 3,
+    nombre: 'Personaje 3',
+    descripcion: 'Descripción del personaje 3',
+  },
+]
+
+const click = (nombre) => {
+  console.log(`Hola ${nombre}`)
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Personajes</h1>
+    <ul>
+      <li v-for="personaje in personajes" :key="personaje.id">
+        <h2 @click="click(personaje.nombre)">{{ personaje.nombre }}</h2>
+        <p>{{ personaje.descripcion }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
